22517f8dbfb5bb25f0933d7a5ca7000f46ae9c40
Cruz Nunez <nunez.a.cruz@gmail.com>
2019-01-09 15:34:43 -0500

    Change offense type column name. Rm offense type validation

--- app/models/offense.rb
+++ app/models/offense.rb
@@ -2,7 +2,6 @@ class Offense < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :street_address, presence: true
-  validates :type, presence: true
   validates :status, presence: true
 
   has_many :contacts
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_175744) do
+ActiveRecord::Schema.define(version: 2019_01_09_203207) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "type"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2019_01_09_175744) do
   end
 
   create_table "offenses", force: :cascade do |t|
-    t.boolean "type"
+    t.boolean "ftp"
     t.string "first_name"
     t.string "last_name"
     t.string "middle_name"
