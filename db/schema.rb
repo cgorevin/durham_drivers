453b4633c701869e6d3a630950a6ecdd765b4cf7
Cait Gorevin <caitlin.gorevin@gmail.com>
2019-01-10 10:12:48 -0500

    Run migrate command to bring in contact history model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_01_09_203207) do
 
   create_table "contact_histories", force: :cascade do |t|
