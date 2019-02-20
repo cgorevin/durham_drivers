ccb49db1d6a246d30383508d080a48a87294d065
Cruz Nunez <nunez.a.cruz@gmail.com>
2019-02-20 11:30:46 -0500

    Fix params of panel form

--- app/controllers/offenses_controller.rb
+++ app/controllers/offenses_controller.rb
@@ -3,8 +3,7 @@ class OffensesController < ApplicationController
 
   before_action :set_params, only: [:index, :group, :group_update]
   before_action :set_offense, only: [:show, :edit, :update]
-  # before_action :authenticate_admin!, :except => [:index]
-  before_action :authenticate_admin!, only: [:new, :index, :show, :edit, :group]
+  before_action :authenticate_admin!, only: [:new, :index, :show, :edit, :group, :panel]
 
   def index
     @group = params[:g]
