d89fb20e94483d98d35a86adce6590c4210de728
Cruz Nunez <nunez.a.cruz@gmail.com>
2019-02-25 13:05:52 -0500

    Refactor contact_time method

--- app/models/contact_history.rb
+++ app/models/contact_history.rb
@@ -4,11 +4,7 @@ class ContactHistory < ApplicationRecord
 
   def contact_time
     time_of_contact = self.created_at
-
-    if contact.method == 'email'
-      time_of_contact.strftime 'Emailed on %m/%d/%Y %l:%M:%S %P'
-    else
-      time_of_contact.strftime 'Texted on %m/%d/%Y %l:%M:%S %P'
-    end
+    method = contact.method == 'email' ? 'Emailed' : 'Texted'
+    time_of_contact.strftime "#{method} on %m/%d/%Y %l:%M:%S %P"
   end
 end
