a41204f75736632bdf8d408f9f581e6c4483264b
Cruz Nunez <nunez.a.cruz@gmail.com>
2019-03-11 17:57:51 -0400

    clean up relief message model

--- app/models/relief_message.rb
+++ app/models/relief_message.rb
@@ -13,99 +13,10 @@ class ReliefMessage < ApplicationRecord
   validates :body, presence: true
   validates :token, presence: true, uniqueness: true
 
-  # 1. FTA dismissed only               (fta approved)
-  # 2. FTC eliminated only              (ftp approved)
-  # 3. FTA dismissed and FTC eliminated (fta and ftp approved)
-  # 4. FTA dismissed and FTC still pending (fta approved && fta)
-  # 5. FTC still pending only           (ftp pending)
-  # 6. FTC remittance denied            (ftp denied) << no letters for these
-  # remember that fta will always be approved, eliminates the possibility of fta being denied
-  # fta approved                (1)
-  # ftp pending                 (5)
-  # ftp approved                (2)
-  # ftp denied                  (6)
-  # fta approved, ftp approved  (3)
-  # fta approved, ftp pending   (4)
-  # fta approved, ftp denied    (missing)
   def generate_email(format = 'html')
-    # we must determine if all the offenses are fta or ftp or both
-    # we must determine if all the offenses are approved/pending/denied
-    message_6 format
-    # if offenses.all?(&:fta?)
-    #   # render message 1
-    #   message_1 format
-    # elsif offenses.all?(&:ftp?)
-    #   if offenses.all?(&:pending?)
-    #     # pending => message 5
-    #     message_5 format
-    #   elsif offenses.all?(&:approved?)
-    #     # approved => message 2
-    #     message_2 format
-    #   elsif offenses.all?(&:denied?)
-    #     # denied => message 6 (no such thing as message 6)
-    #   end
-    # elsif offenses.any?(&:fta?) && offenses.any?(&:ftp?)
-    #   ftps = offenses.select(&:ftp?)
-    #   if ftps.all?(&:approved?)
-    #     # ftp approved => message 3
-    #     message_3 format
-    #   elsif ftps.all?(&:pending?)
-    #     # ftp pending => message 4
-    #     message_4 format
-    #   elsif ftps.all?(&:denied?)
-    #     # ftp denied => missing
-    #   end
-    # end
-  end
-
-  def message_1(format)
-    host = Rails.env.production? ? 'second-chance-driving.herokuapp.com' : 'localhost:3000'
-    https = Rails.env.production?
-    renderer = ApplicationController.renderer.new(http_host: host, https: https)
-    renderer.render(
-      "contact_mailer/message_1.#{format}", layout: nil, locals: { offenses: offenses }
-    )
-  end
-
-  def message_2(format)
-    host = Rails.env.production? ? 'second-chance-driving.herokuapp.com' : 'localhost:3000'
-    https = Rails.env.production?
-    renderer = ApplicationController.renderer.new(http_host: host, https: https)
-    renderer.render(
-      "contact_mailer/message_2.#{format}", layout: nil, locals: { offenses: offenses }
-    )
-  end
-
-  def message_3(format)
-    host = Rails.env.production? ? 'second-chance-driving.herokuapp.com' : 'localhost:3000'
-    https = Rails.env.production?
-    renderer = ApplicationController.renderer.new(http_host: host, https: https)
-    renderer.render(
-      "contact_mailer/message_3.#{format}", layout: nil, locals: { offenses: offenses }
-    )
-  end
-
-  def message_4(format)
-    host = Rails.env.production? ? 'second-chance-driving.herokuapp.com' : 'localhost:3000'
-    https = Rails.env.production?
-    renderer = ApplicationController.renderer.new(http_host: host, https: https)
-    renderer.render(
-      "contact_mailer/message_4.#{format}", layout: nil, locals: { offenses: offenses }
-    )
-  end
-
-  def message_5(format)
-    host = Rails.env.production? ? 'second-chance-driving.herokuapp.com' : 'localhost:3000'
-    https = Rails.env.production?
-    renderer = ApplicationController.renderer.new(http_host: host, https: https)
-    renderer.render(
-      "contact_mailer/message_5.#{format}", layout: nil, locals: { offenses: offenses }
-    )
-  end
-
-  def message_6(format)
-    host = Rails.env.production? ? 'secondchancedriving.org' : 'localhost:3000'
-    https = Rails.env.production?
+    production = Rails.env.production?
+    host = production ? 'secondchancedriving.org' : 'localhost:3000'
+    https = production
     renderer = ApplicationController.renderer.new http_host: host, https: https
     renderer.render(
       "contact_mailer/relief_message.#{format}", layout: nil, locals: { offenses: offenses }
