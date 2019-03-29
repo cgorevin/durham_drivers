2a8558b8b325ff3764588dc1491bc080e9cbee10
Cruz Nunez <nunez.a.cruz@gmail.com>
2019-03-29 12:49:57 -0400

    only use needed bootstrap sprockets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,45 @@
 //= require rails-ujs
-//= require jquery3
+//= require jquery
 //= require popper
-//= require bootstrap-sprockets
-//= require jquery_ujs
+
+// require bootstrap-sprockets
+// require bootstrap
+//= require bootstrap/util
+//= require bootstrap/alert
+//= require bootstrap/button
+//= require bootstrap/tooltip
+
 //= require turbolinks
 //= require_tree .
+
+// bootstrap sprockets (149.6KB) (loads all individually)
+  // util (6.0KB)
+  // alert (5.6KB) (needs util to work)
+  // button (5.8KB) (doesn't need util)
+  // carousel (19.9KB)
+  // collapse (13.1KB)
+  // dropdown (18.0KB)
+  // modal (19.8KB)
+  // tooltip (26.1KB) (needs util to work)
+  // popover (7.5KB)
+  // scrollspy (11.6KB)
+  // tab (8.2KB)
+  // toast (7.7KB)
+  // boostrap sprockets (277B)
+
+// bootstrap (129KB) (loads all in one file)
+
+// custom bootstrap require (32.1KB)
+  // util (6.0KB) << yes
+  // alert (5.6KB) << yes
+  // button (5.8KB) << yes
+  // carousel (19.9KB)
+  // collapse (13.1KB)
+  // dropdown (18.0KB)
+  // modal (19.8KB)
+  // tooltip (26.1KB) << yes
+  // popover (7.5KB)
+  // scrollspy (11.6KB)
+  // tab (8.2KB)
+  // toast (7.7KB)
+  // boostrap sprockets (277B)
