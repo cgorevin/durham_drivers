2809a023bc1ea347c99471faaef0512d84b2abc8
Cruz Nunez <nunez.a.cruz@gmail.com>
2019-04-01 10:17:46 -0400

    rm use of bootstrap/util

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,7 @@
 //= require rails-ujs
 //= require jquery
-//= require bootstrap/util
 //= require bootstrap/button
 //= require turbolinks
 //= require_tree .
 
 // we only need bootstrap/button on pages with 'data-toggle="buttons"''
-// we only need bootstrap/tooltip on pages with 'data-toggle="tooltip"' (contacts#show, offenses#show) (maybe we could remove tooltips)
-// we only need popper on pages on pages with 'data-toggle="tooltip"'' (contacts#show, offenses#show) (maybe we could remove tooltips)
-// tooltips are only used on admin pages
