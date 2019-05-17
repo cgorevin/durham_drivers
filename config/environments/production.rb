ff096d3623be3d743944339167bbedd7bd7f9ffc
Cruz Nunez <nunez.a.cruz@gmail.com>
2019-05-17 14:48:10 -0400

    increase cache ttl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
   # serve assets under public with cache ttl
   config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{(2.days + 7.hours + 33.minutes + 20.seconds).to_i}"
+    'Cache-Control' => "public, max-age=#{(2.days + 10.hours + 20.minutes).to_i}"
   }
 
   # Eager load code on boot. This eager loads most of Rails and
